527c54f4eaed7c777c008204ff747ce19c8e3bfa
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2022-01-29 23:59:07 +0100

    Added mongoose and connected to the database

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql').graphqlHTTP;
 const schema = require("./schema/schema");
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.use(
 	'/graphql',
@@ -12,4 +16,6 @@ app.use(
 	})
 );
 
-app.listen(4000, () => console.log("Server is listening on port 4000"))
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
